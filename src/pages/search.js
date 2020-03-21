98396b19ebb43fc07528a52dd498c430d1d14f74
hendrikniemann <h.neverman@web.de>
2020-03-21 21:53:35 +0100

    Add search component

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,11 +1,19 @@
-import { Heading } from "grommet"
+import * as React from "react"
+import { Box, Heading } from "grommet"
+import { InstantSearch } from "react-instantsearch-dom"
+import algoliasearch from "algoliasearch/lite"
+
+const searchClient = algoliasearch("Your")
 
 export default function Search() {
   return (
     <Box>
-      <Heading>Mein Fachgebiet ist:</Heading>
-      <Heading>Und ich interessiere mich für:</Heading>
-      <Heading>Mein Kurs-Niveau ist:</Heading>
+      <InstantSearch>
+        <Heading>Mein Fachgebiet ist:</Heading>
+
+        <Heading>Und ich interessiere mich für:</Heading>
+        <Heading>Mein Kurs-Niveau ist:</Heading>
+      </InstantSearch>
     </Box>
   )
 }
