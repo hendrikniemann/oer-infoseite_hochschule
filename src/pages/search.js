21831dad89dcecc2d1c46ff80cb8e73c53512f1f
hendrikniemann <h.neverman@web.de>
2020-03-22 12:52:33 +0100

    Implement basic version of the search

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,20 +1,33 @@
 import * as React from "react"
 import { Box, Heading } from "grommet"
-import { SearchBox, InstantSearch } from "react-instantsearch-dom"
+import {
+  RefinementList,
+  InstantSearch,
+  Hits,
+  SearchBox,
+} from "react-instantsearch-dom"
 import algoliasearch from "algoliasearch/lite"
+import Layout from "../components/layout"
 
-const searchClient = algoliasearch("Your")
+const searchClient = algoliasearch(
+  "8U7CL41ANW",
+  "752e9976177a6f7ee110ea71773cb9c4"
+)
 
 export default function Search() {
   return (
-    <Box>
-      {/* <InstantSearch searchClient={searchClient}>
-        <SearchBox />
+    <Layout>
+      <InstantSearch indexName="offers" searchClient={searchClient}>
+        <SearchBox attribute="Title" />
         <Heading>Mein Fachgebiet ist:</Heading>
+        <RefinementList attribute="Fachgebiet (Stichpunkte, nicht Fächer)" />
 
         <Heading>Und ich interessiere mich für:</Heading>
         <Heading>Mein Kurs-Niveau ist:</Heading>
-      </InstantSearch> */}
-    </Box>
+        <Hits />
+      </InstantSearch>
+    </Layout>
   )
 }
+
+function Hit(props) {}
